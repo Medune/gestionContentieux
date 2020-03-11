6fe4837a9243015ac3c151fccd2bc8c097caca6e
Maxime <maxime2.goguet@gmail.com>
2020-03-11 17:06:25 +0100

    LE SERVEUR MARCHE !

--- src/main/java/com/inti/repository/UtilisateurRepository.java
+++ src/main/java/com/inti/repository/UtilisateurRepository.java
@@ -1,26 +1,12 @@
-<<<<<<< HEAD
  package com.inti.repository;
  
  import org.springframework.data.jpa.repository.JpaRepository;
  import org.springframework.stereotype.Repository;
- 
- import com.inti.entities.Utilisateur;
+
+import com.inti.entities.Utilisateur;
  
  @Repository
  public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {  // Long fait référence au type de l'id
  
+	 Utilisateur findOneByUsername(String username);
 }
-=======
-package com.inti.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.inti.entities.Utilisateur;
-
-@Repository
-public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> { // Long fait référence au type de l'id
-
-}
- 
->>>>>>> 05582882387d7ebcb1b1f37f9223fff38a7f2775
