5d71ff1817931eff5ef3307452439a46a0a64d1a
Chocola-sama <chocola_tahiti@hotmail.fr>
2020-03-11 16:34:16 +0100

    3e update après le bordel de Ous-sama

--- src/main/java/com/inti/entities/Role.java
+++ src/main/java/com/inti/entities/Role.java
@@ -8,7 +8,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 
 @Entity
 public class Role implements Serializable {
--- src/main/java/com/inti/service/impl/UtilisateurService.java
+++ src/main/java/com/inti/service/impl/UtilisateurService.java
@@ -1,38 +1,44 @@
-//package com.inti.service.impl;
-//
-//import java.util.List;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import com.inti.entities.Utilisateur;
-//import com.inti.repository.UtilisateurRepository;
-//import com.inti.service.interfaces.IUtilisateurService;
-//
-//@Service
-//public class UtilisateurService implements IUtilisateurService {
-//
-//	@Autowired
-//	UtilisateurRepository utilisateurRepository;
-//	
-//	@Override
-//	public List<Utilisateur> findAll() {
-//		return utilisateurRepository.findAll();
-//	}
-//
-//	@Override
-//	public Utilisateur findOne(Long id) {
-//		return utilisateurRepository.findOne(id);
-//	}
-//
-//	@Override
-//	public Utilisateur save(Utilisateur user) {
-//		return utilisateurRepository.save(user);
-//	}
-//
-//	@Override
-//	public void delete(Long id) {
-//		utilisateurRepository.delete(id);
-//	}
-//
-//}
+package com.inti.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.inti.entities.Utilisateur;
+import com.inti.repository.UtilisateurRepository;
+import com.inti.service.interfaces.IUtilisateurService;
+
+@Service
+public class UtilisateurService implements IUtilisateurService {
+
+	@Autowired
+	UtilisateurRepository utilisateurRepository;
+	
+	@Override
+	public List<Utilisateur> findAll() {
+		return utilisateurRepository.findAll();
+	}
+
+	@Override
+	public Utilisateur findOne(Long id) {
+		return utilisateurRepository.findOne(id);
+	}
+	
+	@Override
+	public Utilisateur findOneByUsername(String username) {
+		return utilisateurRepository.findOneByUsername(username);
+	}
+
+	@Override
+	public Utilisateur save(Utilisateur user) {
+		return utilisateurRepository.save(user);
+	}
+
+	@Override
+	public void delete(Long id) {
+		utilisateurRepository.delete(id);
+	}
+
+
+}
