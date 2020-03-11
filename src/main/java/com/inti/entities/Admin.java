0acca7cd6281c6f0947ab4437e98c49b6c711aa5
manselme <victor.bataillon@gmail.com>
2020-03-11 15:05:39 +0100

    admin+respo

---
+++ src/main/java/com/inti/entities/Admin.java
@@ -0,0 +1,13 @@
+package com.inti.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Admin extends Utilisateur{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	
+}
---
+++ src/main/java/com/inti/entities/ResponsableJuridique.java
@@ -0,0 +1,10 @@
+package com.inti.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class ResponsableJuridique  extends Utilisateur{
+
+	private static final long serialVersionUID = 1L;
+
+}
